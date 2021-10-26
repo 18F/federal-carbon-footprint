a7411d13e5568fca16d28203326a40bb767cb3b4
Daniel Naab <dan@crushingpennies.com>
2021-10-26 12:21:55 -0500

    Add favicon.ico relative to base url.

--- web/src/routes/__layout.svelte
+++ web/src/routes/__layout.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
   import '../styles/app.scss';
+  import { base } from '$app/paths';
   import BetaBanner from '$lib/components/BetaBanner.svelte';
   import Banner from '$lib/components/Banner.svelte';
   import Header from '$lib/components/Header.svelte';
   import Footer from '$lib/components/Footer.svelte';
 </script>
+
+<svelte:head>
+  <link rel="icon" href={`${base}/favicon.ico`} />
+</svelte:head>
 
 <BetaBanner />
 <Banner />

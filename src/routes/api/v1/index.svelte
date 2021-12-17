10a89be93c270fc8e91e6c0eeda08184ecd2eecf
Daniel Naab <dan@crushingpennies.com>
2021-12-16 18:17:13 -0600

    Downgrade SvelteKit to 1.0.0-next.199 to avoid bug: https://github.com/sveltejs/kit/issues/2962

--- src/routes/api/v1/index.svelte
+++ src/routes/api/v1/index.svelte
@@ -11,34 +11,4 @@
 </svelte:head>
 
 <div class="grid-container">
-  <div class="grid-row">
-    <div class="grid-col-auto">
-      <table class="usa-table usa-table--borderless">
-        <caption>
-          The data visualized on this site is available for download.
-        </caption>
-        <thead>
-          <tr>
-            <th scope="col">Description</th>
-            <th scope="col">Download</th>
-            <th scope="col">Update frequency</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td> Total aggregate fuel usage </td>
-            <td>
-              <a href={`${base}/api/v1/fuel-type-usage.json`} target="_blank"
-                ><span class="usa-tag bg-primary">JSON</span></a
-              >
-              <a href={`${base}/api/v1/fuel-type-usage.csv`} target="_blank"
-                ><span class="usa-tag bg-primary">CSV</span></a
-              >
-            </td>
-            <td>Annual</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
 </div>
